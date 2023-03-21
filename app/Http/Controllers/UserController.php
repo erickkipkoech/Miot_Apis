b4fabbb2a10a787b430d601cd3cdca6c5dba26af
erickkipkoech <cheruiyotkipkoech612@gmail.com>
2023-03-21 20:46:19 +0300

    added login api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    //
+    //Create Users
     public function register(Request $request){
         $fields=$request->validate([
             'name'=>'required|string',
@@ -27,6 +27,37 @@ class UserController extends Controller
         ];
         return response($response,201);
     }
+//Login users
+    public function login(Request $request){
+        $fields=$request->validate([
+            'email'=>'email|required',
+            'password'=>'required|'
+        ]);
+        //Check email in the database
+        $user=User::where('email',$fields['email'])->first();
+        //check password
+        if(!$user || !Hash::check($fields['password'],
+        $user->password)){
+            return response([
+                'message'=>'Invalid credentials'
+            ],401);
+        }
+        $token=$user->createToken('miot')->plainTextToken;
+        $response=[
+            'user'=>$user,
+            'token'=>$token
+        ];
+        return response($response,200);
+    }
+
+    // public function logout(){
+    //     auth()->user()->tokens()->delete();
+    //     return[
+    //         'message'=>'Logged out'
+    //     ];
+    // }
+
+
     public function users(string $name){
         return User::find($name);
     }
